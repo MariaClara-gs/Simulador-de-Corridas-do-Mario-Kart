576b5e0a11665cb9283c17afbae9c576d58e3745
Maria Clara <mariaclara2016085@gmail.com>
2026-03-13 18:44:35 -0300

    feat: functions to main operations and adding player objects

---
+++ index.js
@@ -0,0 +1,38 @@
+//personagens pre-determinados para o jogo
+const player1 = {
+  nome: "Mario",
+  velocidade: 4,
+  manobrabilidade: 3,
+  poder: 3,
+  pontos: 0,
+};
+
+const player2 = {
+  nome: "Luigi",
+  velocidade: 3,
+  manobrabilidade: 4,
+  poder: 4,
+  pontos: 0,
+};
+
+//funcao para rolar dados
+async function rollDice() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+async function playRaceEngine(character1, character2) {
+  for (let round = 1; round <= 5; round++) {
+    console.log(`🏁 Rodada ${round}`);
+
+    //sortear bloco
+  }
+}
+
+//funcao principal auto-invocavel
+(async function main() {
+  console.log(
+    `🏁🚨 Corrida entre ${player1.nome} e ${player2.nome} começando...\n`,
+  );
+
+  await playRaceEngine(player1, player2);
+})();
